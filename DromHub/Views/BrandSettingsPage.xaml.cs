00edc700ef3234b8fb94acbd8c53e831a3d95063
stulevtoday <danilstulev@gmail.com>
2025-10-06 18:34:07 +1000

    Desing settings page

--- DromHub/Views/BrandSettingsPage.xaml.cs
+++ DromHub/Views/BrandSettingsPage.xaml.cs
@@ -1,8 +1,36 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+
 namespace DromHub.Views
 {
     public sealed partial class BrandSettingsPage : Page
     {
-        public BrandSettingsPage() => InitializeComponent();
+        private readonly IReadOnlyList<Expander> _accordion;
+
+        public BrandSettingsPage()
+        {
+            InitializeComponent();
+
+            _accordion = new[] { ExpCore, ExpAliases, ExpPricing, ExpUser };
+
+            foreach (var ex in _accordion)
+                ex.Expanding += Accordion_Expanding;
+
+            Loaded += (_, __) =>
+            {
+                // открыть первый, остальные закрыть
+                ExpCore.IsExpanded = true;
+                for (int i = 1; i < _accordion.Count; i++)
+                    _accordion[i].IsExpanded = false;
+            };
+        }
+
+        private void Accordion_Expanding(Expander sender, ExpanderExpandingEventArgs args)
+        {
+            foreach (var ex in _accordion)
+                if (!ReferenceEquals(ex, sender))
+                    ex.IsExpanded = false;
+        }
     }
 }
